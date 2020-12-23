c5965cdd0949f9c0e6a8ffcb65cfaf3afe5cdf22
Ubuntu <ubuntu@ip-172-31-79-133.ec2.internal>
2020-12-23 13:11:18 +0000

    process orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,45 @@ Rails.application.routes.draw do
   get 'cart/index'
 
   resources :items
-root 'static_pages#home'
-
-get '/help' => 'static_pages#help'
-get '/about' => 'static_pages#about'
-
- 
-# get 'static_pages/home'
-# get 'static_pages/help'
-# get 'static_pages/about'
-
-  resources :registrations
+  #get 'static_pages/home'
+  root 'static_pages#home'
+  #get 'static_pages/help'
   
-get '/login' => 'user#login'
-get '/logout' => 'user#logout'
-get '/cart/:id', to: 'cart#add'
-get '/cart', to: 'cart#index'
-get '/clearcart', to: 'cart#clearCart'
+  get '/help' => 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  
+  get '/login', to: 'user#login'
+  get '/logout', to: 'user#logout'
+  
+  
+  get '/clearcart', to: 'cart#clearCart'
+  get '/cart', to: 'cart#index'
+  get '/cart/:id', to: 'cart#add'
+  get '/cart/remove/:id' => 'cart#remove'
+  get '/cart/decrease/:id' => 'cart#decrease'
+  
+  #root :to => 'static_pages#home'
+  root :to => 'site#home'
+  
+  get 'orderitems/index'
 
+  get 'orderitems/show'
 
+  get 'orderitems/new'
 
-  #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'orderitems/edit'
+
+  resources :orders do 
+    resources:orderitems
+  end
+  
+  devise_for :users do 
+    resources :orders
+  end
+  
+  get '/checkout' => 'cart#createOrder'
+  get '/paid/:id' => 'static_pages#paid'
+  #get 'static_pages/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
